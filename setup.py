e41a6e2b869472e1b351aac06241ade5c41ad8e5
matt <matthewharrison@gmail.com>
2011-01-24 13:33:20 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-extra_requires = []
-
 setup(name='NoseGoat',
       version='0.1',
       author='Matt Harrison',
       author_email = 'matthewharrison@gmail.com',
       description = 'Use the goat Luke!',
+      long_description = 'Nose Plugin for goat enableation',
+      platforms = 'goats eat anything',
+      url='https://github.com/mattharrison/NoseGoat',
       license = 'PSF',
       py_modules=['nosegoat'],
-      install_requires = ['nose>=0.10.0'] + extra_requires,
+      install_requires = ['nose>=0.10.0'],
       entry_points = { 'nose.plugins.0.10': ['goat = nosegoat:GoatPlugin']})
